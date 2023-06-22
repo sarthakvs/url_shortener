f37f25ed69627670eee83680b341c81c52d10500
sarthakvs <98168713+sarthakvs@users.noreply.github.com>
2023-06-22 14:19:21 +0530

    Add files via upload

---
+++ public/client.js
@@ -0,0 +1,78 @@
+const form = document.getElementById('url-form');
+const fullUrlInput = document.getElementById('fullUrl');
+const shortUrlElement = document.getElementById('shortUrl');
+const urlTableBody = document.getElementById('urlTableBody');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const fullUrl = fullUrlInput.value;
+
+  try {
+    const response = await fetch('/api/shorten', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ fullUrl })
+    });
+    const data = await response.json();
+    const shortUrl = data.shortUrl;
+    shortUrlElement.innerHTML = `Short URL: <a href="${shortUrl}" target="_blank">${shortUrl}</a>`;
+
+    // Add the new URL to the table dynamically
+    const row = document.createElement('tr');
+    const fullUrlCell = document.createElement('td');
+    const shortUrlCell = document.createElement('td');
+
+    fullUrlCell.appendChild(createLink(fullUrl));
+    shortUrlCell.appendChild(createLink(shortUrl));
+
+    row.appendChild(fullUrlCell);
+    row.appendChild(shortUrlCell);
+
+    urlTableBody.appendChild(row);
+  } catch (error) {
+    console.error('Error shortening URL:', error);
+  }
+});
+
+// Function to create a clickable link
+function createLink(url) {
+  const link = document.createElement('a');
+  link.href = url;
+  link.textContent = url;
+  link.target = '_blank';
+  return link;
+}
+
+// Function to fetch and display database contents
+async function fetchDatabaseContents() {
+  try {
+    const response = await fetch('/api/urls');
+    const data = await response.json();
+    const urls = data.urls;
+    const tableBody = document.getElementById('urlTableBody');
+
+    // Clear existing table rows
+    tableBody.innerHTML = '';
+
+    // Insert new rows into the table
+    urls.forEach((url) => {
+      const row = document.createElement('tr');
+      const fullUrlCell = document.createElement('td');
+      const shortUrlCell = document.createElement('td');
+
+      fullUrlCell.appendChild(createLink(url.fullUrl));
+      shortUrlCell.appendChild(createLink(url.shortUrl));
+
+      row.appendChild(fullUrlCell);
+      row.appendChild(shortUrlCell);
+      tableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error('Error fetching database contents:', error);
+  }
+}
+
+// Fetch and display database contents when the page loads
+window.addEventListener('load', fetchDatabaseContents);
